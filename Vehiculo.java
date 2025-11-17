8bc5d35ee9897fabfb47f1a2f8bec6f8d04192ec
Carogc27 <72954461+Carogc27@users.noreply.github.com>
2025-11-16 22:14:10 -0500

    Add files via upload

---
+++ Vehiculo.java
@@ -0,0 +1,16 @@
+package Modelo;
+
+public class Vehiculo {
+    // 🔠 Atributos
+    private String placa;
+
+    // 🔨 Constructor
+    public Vehiculo(String placa) {
+        this.placa = placa;
+    }
+
+    // 🗣️ Getters
+    public String getPlaca() {
+        return placa;
+    }
+}
